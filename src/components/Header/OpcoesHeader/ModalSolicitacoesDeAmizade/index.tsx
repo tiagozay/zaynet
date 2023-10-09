846bf780e99ccff8d1e52dcde75b40bcc54a8f55
Tiago zay <tiagozay@gmail.com>
2023-10-09 17:32:23 -0300

    Criando modal de solicitações de amizade
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

---
+++ src/components/Header/OpcoesHeader/ModalSolicitacoesDeAmizade/index.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import './ModalSolicitacoesDeAmizade.css';
+
+interface ModalSolicitacoesDeAmizadeProps {
+    aberto: boolean,
+    clickFora: Function
+}
+
+export default function ModalSolicitacoesDeAmizade({ aberto, clickFora }: ModalSolicitacoesDeAmizadeProps) {
+    return (
+
+        aberto ?
+            <>
+                <div className='modal-overlay' onClick={() => clickFora()}></div>
+                <nav id='navSolicitacoesDeAmizade' className='navOpcoesHeader'>
+                    <h3 id='navSolicitacoesDeAmizade__titulo'>
+                        Solicitações de amizade
+                    </h3>
+                    <ul id="listaDeSolicitacoesDeAmizade">
+                        <li id='listaDeSolicitacoesDeAmizade__solicitacao'>
+                            <img src="/imagensDinamicas/perfil.jpg" alt="Foto de perfil" id="solicitacao__perfil" />
+                            <div id='solicitacao__informacoesPessoa'>
+                                <p id='solicitacao__informacoesPessoa__nome'>Tiago zay</p>
+                                <p id='solicitacao__informacoesPessoa__amigosEmComum'>25 amigos em comum</p>
+                            </div>
+                            <div id='solicitacao__opcoes'>
+                                <button id='solicitacao__opcoes__botaoRemover'>Remover</button>
+                                <button id='solicitacao__opcoes__botaoAceitar'>Aceitar</button>
+                            </div>
+                        </li>
+                        <li id='listaDeSolicitacoesDeAmizade__solicitacao'>
+                            <img src="/imagensDinamicas/perfil2.jpg" alt="Foto de perfil" id="solicitacao__perfil" />
+                            <div id='solicitacao__informacoesPessoa'>
+                                <p id='solicitacao__informacoesPessoa__nome'>Ervino zay</p>
+                                <p id='solicitacao__informacoesPessoa__amigosEmComum'>102 amigos em comum</p>
+                            </div>
+                            <div id='solicitacao__opcoes'>
+                                <button id='solicitacao__opcoes__botaoRemover'>Remover</button>
+                                <button id='solicitacao__opcoes__botaoAceitar'>Aceitar</button>
+                            </div>
+                        </li>
+                        <li id='listaDeSolicitacoesDeAmizade__solicitacao'>
+                            <img src="/imagensDinamicas/perfil.jpg" alt="Foto de perfil" id="solicitacao__perfil" />
+                            <div id='solicitacao__informacoesPessoa'>
+                                <p id='solicitacao__informacoesPessoa__nome'>Tiago zay</p>
+                                <p id='solicitacao__informacoesPessoa__amigosEmComum'>25 amigos em comum</p>
+                            </div>
+                            <div id='solicitacao__opcoes'>
+                                <button id='solicitacao__opcoes__botaoRemover'>Remover</button>
+                                <button id='solicitacao__opcoes__botaoAceitar'>Aceitar</button>
+                            </div>
+                        </li>
+                        <li id='listaDeSolicitacoesDeAmizade__solicitacao'>
+                            <img src="/imagensDinamicas/perfil.jpg" alt="Foto de perfil" id="solicitacao__perfil" />
+                            <div id='solicitacao__informacoesPessoa'>
+                                <p id='solicitacao__informacoesPessoa__nome'>Tiago zay</p>
+                                <p id='solicitacao__informacoesPessoa__amigosEmComum'>25 amigos em comum</p>
+                            </div>
+                            <div id='solicitacao__opcoes'>
+                                <button id='solicitacao__opcoes__botaoRemover'>Remover</button>
+                                <button id='solicitacao__opcoes__botaoAceitar'>Aceitar</button>
+                            </div>
+                        </li>
+                        <li id='listaDeSolicitacoesDeAmizade__solicitacao'>
+                            <img src="/imagensDinamicas/perfil2.jpg" alt="Foto de perfil" id="solicitacao__perfil" />
+                            <div id='solicitacao__informacoesPessoa'>
+                                <p id='solicitacao__informacoesPessoa__nome'>Ervino zay</p>
+                                <p id='solicitacao__informacoesPessoa__amigosEmComum'>102 amigos em comum</p>
+                            </div>
+                            <div id='solicitacao__opcoes'>
+                                <button id='solicitacao__opcoes__botaoRemover'>Remover</button>
+                                <button id='solicitacao__opcoes__botaoAceitar'>Aceitar</button>
+                            </div>
+                        </li>
+                        <li id='listaDeSolicitacoesDeAmizade__solicitacao'>
+                            <img src="/imagensDinamicas/perfil.jpg" alt="Foto de perfil" id="solicitacao__perfil" />
+                            <div id='solicitacao__informacoesPessoa'>
+                                <p id='solicitacao__informacoesPessoa__nome'>Tiago zay</p>
+                                <p id='solicitacao__informacoesPessoa__amigosEmComum'>25 amigos em comum</p>
+                            </div>
+                            <div id='solicitacao__opcoes'>
+                                <button id='solicitacao__opcoes__botaoRemover'>Remover</button>
+                                <button id='solicitacao__opcoes__botaoAceitar'>Aceitar</button>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+            </>
+            :
+            <></>
+    )
+}
--- src/components/Header/OpcoesHeader/index.tsx
+++ src/components/Header/OpcoesHeader/index.tsx
@@ -1,15 +1,50 @@
 import React from 'react';
 import './OpcoesHeader.css';
+import { useState } from 'react';
+import ModalSolicitacoesDeAmizade from './ModalSolicitacoesDeAmizade';
 
 export default function OpcoesHeader() {
+
+    const [modalSolicitacoesDeAmizadeAberto, setModalSolicitacoesDeAmizadeAberto] = useState<boolean>(false);
+
+    function clickTecla(e: KeyboardEvent)
+    {
+        if(e.key === 'Escape'){
+            setModalSolicitacoesDeAmizadeAberto(false);
+        }
+    }
+
+    function fecharModais()
+    {
+        setModalSolicitacoesDeAmizadeAberto(false);
+    }
+
+    function clickSolicitacoesDeAmizade()
+    {
+        setModalSolicitacoesDeAmizadeAberto(!modalSolicitacoesDeAmizadeAberto);
+    }
+
+    document.addEventListener('keydown', clickTecla);
+
     return (
         <div id='opcoesHeader'>
-            <i className='material-symbols-outlined opcoesHeaderIcone'>group_add</i>
-            <i className='material-symbols-outlined opcoesHeaderIcone'>chat_bubble</i>
-            <i className='material-symbols-outlined opcoesHeaderIcone'>notifications</i>
+            <i className={`
+                material-symbols-outlined
+                opcoesHeaderIcone 
+                ${modalSolicitacoesDeAmizadeAberto && "opcoesHeaderIcone__ativo"}
+            `} onClick={clickSolicitacoesDeAmizade}>group_add</i>
+            <i className={`
+                material-symbols-outlined 
+                opcoesHeaderIcone
+            `}>chat_bubble</i>
+            <i className={`
+                material-symbols-outlined 
+                opcoesHeaderIcone
+            `}>notifications</i>
             <i className='opcoesHeaderIcone' id='opcoesHeaderIcone__perfil'>
                 <img src="imagensDinamicas/perfil.jpg" alt="" />
             </i>
+            <ModalSolicitacoesDeAmizade aberto={modalSolicitacoesDeAmizadeAberto} clickFora={fecharModais}/>
         </div>
     )
 }
