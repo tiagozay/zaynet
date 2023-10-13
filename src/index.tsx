420c0f4d53c5a9a3f507ab194881bd48f2163a9d
Tiago zay <tiagozay@gmail.com>
2023-10-13 16:51:53 -0300

    Criando feed e componente Publicação
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import './styles/reset.css';
+import './styles/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 root.render(
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Header from '../../components/Header'
-import MenuEsquerdo from '../../components/MenuEsquerdo'
+import React from 'react';
+import Header from '../../components/Header';
+import MenuEsquerdo from '../../components/MenuEsquerdo';
+import Publicacao from '../../components/Publicacao';
+import './home.css';
 
 export default function Home() {
   return (
     <>
-        <Header></Header>
-        <MenuEsquerdo></MenuEsquerdo>
+      <Header></Header>
+      <MenuEsquerdo></MenuEsquerdo>
+
+      <section id='feed'>
+        <Publicacao></Publicacao>
+        <Publicacao></Publicacao>
+      </section>
+
+
     </>
   )
 }
