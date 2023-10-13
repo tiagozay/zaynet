de7f5aac69293127d7a577ed2f4366ffee96a00e
Tiago zay <tiagozay@gmail.com>
2023-10-13 14:12:03 -0300

    Finalizando modal de mensagens
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- src/components/Header/OpcoesHeader/ModalMensagens/index.tsx
+++ src/components/Header/OpcoesHeader/ModalMensagens/index.tsx
@@ -16,6 +16,21 @@ export default function ModalMensagens({ aberto, clickFora }: ModalMensagensProp
                         Mensagens
                     </h3>
                     <ul id="listaDeMensagens">
+                        <li className='listaDeMensagens__mensagem'>
+                            <img src="/imagensDinamicas/perfil.jpg" alt="Foto de perfil" id="mensagem__perfil"/>
+                            <div id='mensagem__informacoesPessoa'>
+                                <p id='mensagem__informacoesPessoa__nome'>Tiago zay</p>
+                                <p id='mensagem__informacoesPessoa__ultimaMensagem'>Vou ir lá de tarde</p>
+                            </div>
+
+                        </li>
+                        <li className='listaDeMensagens__mensagem'>
+                            <img src="/imagensDinamicas/perfil2.jpg" alt="Foto de perfil" id="mensagem__perfil"/>
+                            <div id='mensagem__informacoesPessoa'>
+                                <p id='mensagem__informacoesPessoa__nome'>Ervino zay</p>
+                                <p id='mensagem__informacoesPessoa__ultimaMensagem'>Suka</p>
+                            </div>
+                        </li>
                     </ul>
                 </nav>
             </>
