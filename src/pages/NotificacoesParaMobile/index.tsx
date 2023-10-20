434116025fec7063f12620aceca271e6ab85cf94
Tiago zay <tiagozay@gmail.com>
2023-10-20 17:33:45 -0300

    Criando a tela de notificações para mobile
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- src/pages/NotificacoesParaMobile/index.tsx
+++ src/pages/NotificacoesParaMobile/index.tsx
@@ -5,7 +5,36 @@ import './NotificacoesParaMobile.css';
 export default function NotificacoesParaMobile() {
   return (
     <section id='notificacoesPage' className='espacamentosParaCompensarPartesDoHeader'>
-        <p>Opa</p>
+      <h3 id='notificacoesMobile__titulo'>Notificações</h3>
+      <ul id='notificacoesMobile__notificacoes'>
+        <li className='notificacoesMobile__notificacao'>
+          <img src="/imagensDinamicas/perfil.jpg" alt="Foto de perfil" id="notificacaoMobile__perfil" />
+          <div id='notificacaoMobile__acao'>
+            <p>
+              <span id='notificacaoMobile__acao__autor'>Tiago zay </span>
+              Adicionou uma nova foto.
+            </p>
+          </div>
+        </li>
+        <li className='notificacoesMobile__notificacao'>
+          <img src="/imagensDinamicas/perfil2.jpg" alt="Foto de perfil" id="notificacaoMobile__perfil" />
+          <div id='notificacaoMobile__acao'>
+            <p>
+              <span id='notificacaoMobile__acao__autor'>Ervino zay </span>
+              Comentou 'Suka' em sua publicação.
+            </p>
+          </div>
+        </li>
+        <li className='notificacoesMobile__notificacao'>
+          <img src="/imagensDinamicas/perfil.jpg" alt="Foto de perfil" id="notificacaoMobile__perfil" />
+          <div id='notificacaoMobile__acao'>
+            <p>
+              <span id='notificacaoMobile__acao__autor'>Tiago zay </span>
+              Adicionou uma nova foto.
+            </p>
+          </div>
+        </li>
+      </ul>
     </section>
   )
 }
