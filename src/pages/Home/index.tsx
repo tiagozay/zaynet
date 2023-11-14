f9f8a45c841b306e20924c717eebc0355da1a309
Tiago zay <tiagozay@gmail.com>
2023-11-14 14:37:20 -0300

    Criando input da home de adicionar publicação
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,6 +8,26 @@ export default function Home() {
   return (
     <>
       <section id='feed'>
+
+        <div id='feed_adicionarUmaNovaPublicacao'>
+          <div id='feed_adicionarUmaNovaPublicacao__container'>
+            <img
+              src="./imagensDinamicas/perfil.jpg"
+              alt="Foto perfil"
+              id='feed_adicionarUmaNovaPublicacao__perfil'
+            />
+            <input
+              type="text"
+              id='feed_adicionarUmaNovaPublicacao__input'
+              placeholder='No que você está pensando, Tiago?'
+            />
+          </div>
+          <button id='feed_adicionarUmaNovaPublicacao__btnFotoEVideo'>
+            <img src="./icones/imagemIcone.png" alt="" />
+            Foto/vídeo
+          </button>
+        </div>
+
         <Publicacao></Publicacao>
         <Publicacao></Publicacao>
       </section>
