cf02c0e8e0911cb5d9c95ec7895fe1c7f99a98ab
Tiago zay <tiagozay@gmail.com>
2023-12-26 11:56:30 -0300

    Criando lista de amigos para perfil do usuário
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- src/pages/AmigosPerfilUsuario/index.tsx
+++ src/pages/AmigosPerfilUsuario/index.tsx
@@ -3,6 +3,54 @@ import './AmigosPerfilUsuario.css';
 
 export default function AmigosPerfilUsuario() {
   return (
-    <div>AmigosPerfilUsuario</div>
+    <section id="amigosPerfilDoUsuario__page">
+      <h3 id="amigosPerfilDoUsuario__titulo">Amigos</h3>
+      <ul id='amigosPerfilDoUsuario__listaDeAmigos'>
+        <li className='amigosPerfilDoUsuario__listaDeAmigos__amigo'> 
+          <img 
+            src="./../imagensDinamicas/perfil.jpg" 
+            alt="Perfil usuario" 
+            className='amigosPerfilDoUsuario__listaDeAmigos__amigo__perfil'
+          />
+          <div>
+            <p className='amigosPerfilDoUsuario__listaDeAmigos__amigo__nome'>Pedro almeida</p>
+            <p className='amigosPerfilDoUsuario__listaDeAmigos__amigo__amigosEmComum'>110 amigos em comum</p>
+          </div>
+        </li>
+        <li className='amigosPerfilDoUsuario__listaDeAmigos__amigo'> 
+          <img 
+            src="./../imagensDinamicas/perfil2.jpg" 
+            alt="Perfil usuario" 
+            className='amigosPerfilDoUsuario__listaDeAmigos__amigo__perfil'
+          />
+          <div>
+            <p className='amigosPerfilDoUsuario__listaDeAmigos__amigo__nome'>Cristina souza</p>
+            <p className='amigosPerfilDoUsuario__listaDeAmigos__amigo__amigosEmComum'>96 amigos em comum</p>
+          </div>
+        </li>
+        <li className='amigosPerfilDoUsuario__listaDeAmigos__amigo'> 
+          <img 
+            src="./../imagensDinamicas/perfil2.jpg" 
+            alt="Perfil usuario" 
+            className='amigosPerfilDoUsuario__listaDeAmigos__amigo__perfil'
+          />
+          <div>
+            <p className='amigosPerfilDoUsuario__listaDeAmigos__amigo__nome'>Maria de andrade</p>
+            <p className='amigosPerfilDoUsuario__listaDeAmigos__amigo__amigosEmComum'>120 amigos em comum</p>
+          </div>
+        </li>
+        <li className='amigosPerfilDoUsuario__listaDeAmigos__amigo'> 
+          <img 
+            src="./../imagensDinamicas/perfil.jpg" 
+            alt="Perfil usuario" 
+            className='amigosPerfilDoUsuario__listaDeAmigos__amigo__perfil'
+          />
+          <div>
+            <p className='amigosPerfilDoUsuario__listaDeAmigos__amigo__nome'>André souza</p>
+            <p className='amigosPerfilDoUsuario__listaDeAmigos__amigo__amigosEmComum'>12 amigos em comum</p>
+          </div>
+        </li>
+      </ul>
+    </section>
   )
 }
