06fd70459dc6e04887d9cf1927e9881d9158c717
Tiago zay <tiagozay@gmail.com>
2023-12-26 11:08:55 -0300

    Criando página de vídeos do usuário
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- src/pages/VideosPerfilUsuario/index.tsx
+++ src/pages/VideosPerfilUsuario/index.tsx
@@ -1,7 +1,68 @@
 import React from 'react'
+import './VideosPerfilUsuario.css';
+import { MidiaPublicacaoModel } from '../../models/MidiaPublicacaoModel';
+import { useNavigate } from 'react-router-dom';
 
 export default function VideosPerfilUsuario() {
+
+  const navigate = useNavigate();
+
+  const fotosUsuario = [
+    new MidiaPublicacaoModel(
+      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub1.jpg',
+      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub1.jpg',
+    ),
+    new MidiaPublicacaoModel(
+      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub2.jpg',
+      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub2.jpg',
+    ),
+    new MidiaPublicacaoModel(
+      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub3.jpg',
+      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub3.jpg',
+    ),
+    new MidiaPublicacaoModel(
+      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub4.jpg',
+      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub4.jpg',
+    ),
+    new MidiaPublicacaoModel(
+      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub5.jpg',
+      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub5.jpg',
+    ),
+    new MidiaPublicacaoModel(
+      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub6.mp4',
+      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub6.jpg',
+    )
+  ];
+
+  function aoClicarEmUmVideo(indice: number) {
+    const info = JSON.stringify(
+      { imagensDoCarrossel: fotosUsuario, indiceImagemInicial: indice }
+    );
+    navigate(`/image/${encodeURIComponent(info)}`);
+  }
+
   return (
-    <div>VideosPerfilUsuario</div>
+    <section id="videosPerfilDoUsuario__page">
+      <h3 id="videosPerfilDoUsuario__titulo">Vídeos</h3>
+      <ul id='videosPerfilDoUsuario__listaDeVideos'>
+
+        {
+          fotosUsuario.map((foto, index) => {
+
+            return (
+              <li className='videosPerfilDoUsuario__listaDeVideos__video' key={index} onClick={() => aoClicarEmUmVideo(index)}>
+                <div className='videosPerfilDoUsuario__listaDeVideos__videoOverlay'>
+                  <button className='material-symbols-outlined'>play_circle</button>
+                </div>
+                <img
+                  src={foto.caminhoMidiaMiniatura}
+                  alt="Foto usuário"
+                />
+              </li>
+            )
+          })
+        }
+      </ul>
+    </section>
   )
 }
