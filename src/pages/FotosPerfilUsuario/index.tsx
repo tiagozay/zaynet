fbda76ec8d9117868450ccb6db4f48b0654d176c
Tiago zay <tiagozay@gmail.com>
2023-12-26 10:42:43 -0300

    Criando página das fotos do perfil do usuário
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- src/pages/FotosPerfilUsuario/index.tsx
+++ src/pages/FotosPerfilUsuario/index.tsx
@@ -1,8 +1,67 @@
 import React from 'react';
 import './FotosPerfilUsuario.css';
+import { MidiaPublicacaoModel } from '../../models/MidiaPublicacaoModel';
+import { useNavigate } from 'react-router-dom';
 
 export default function FotosPerfilUsuario() {
+
+  const navigate = useNavigate();
+
+  //Mock das fotos do usuário logado que futuramente virão de outro lugar, como redux
+  const fotosUsuario = [
+    new MidiaPublicacaoModel(
+      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub1.jpg',
+      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub1.jpg',
+    ),
+    new MidiaPublicacaoModel(
+      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub2.jpg',
+      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub2.jpg',
+    ),
+    new MidiaPublicacaoModel(
+      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub3.jpg',
+      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub3.jpg',
+    ),
+    new MidiaPublicacaoModel(
+      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub4.jpg',
+      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub4.jpg',
+    ),
+    new MidiaPublicacaoModel(
+      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub5.jpg',
+      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub5.jpg',
+    ),
+    new MidiaPublicacaoModel(
+      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub6.mp4',
+      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub6.jpg',
+    )
+  ];
+
+  function aoClicarEmUmaImagem(indice: number) {
+    const info = JSON.stringify(
+      { imagensDoCarrossel: fotosUsuario, indiceImagemInicial: indice }
+    );
+    navigate(`/image/${encodeURIComponent(info)}`);
+  }
+
+
   return (
-    <div>FotosPerfilUsuario</div>
+    <section id="fotosPerfilDoUsuario__page">
+      <h3 id="fotosPerfilDoUsuario__titulo">Fotos</h3>
+      <ul id='fotosPerfilDoUsuario__listaDeFotos'>
+
+        {
+          fotosUsuario.map((foto, index) => {
+            return (
+              <li className='fotosPerfilDoUsuario__listaDeFotos__foto' key={index}>
+                <img 
+                  src={foto.caminhoMidiaMiniatura}
+                  alt="Foto usuário"
+                  onClick={() => aoClicarEmUmaImagem(index)}
+                />
+              </li>
+            )
+          })
+        }
+      </ul>
+    </section>
   )
 }
