1277359b4118ff2395bbe5c98d7a63d757eec77c
Tiago zay <tiagozay@gmail.com>
2024-01-05 09:52:36 -0300

    Criando modal de editar publicação compartilhada
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

---
+++ src/components/ModalEditarPublicacaoCompartilhada/index.tsx
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from 'react';
+import './ModalEditarPublicacaoCompartilhada.css';
+import ModalDeConfirmacao from '../ModalDeConfirmacao';
+import Publicacao from '../Publicacao';
+
+interface ModalEditarPublicacaoCompatilhadaProps {
+    modalAberto: boolean,
+    fecharModal: () => void
+}
+
+export default function ModalEditarPublicacaoCompartilhada({ modalAberto, fecharModal }: ModalEditarPublicacaoCompatilhadaProps) {
+
+    //Mock provisório de uma publicação. Posteriormente ela virá do redux ou algo semelhante.
+    const publicacao = {
+        texto: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero, distinctio autem? Magnam autem quisquam voluptates eius cupiditate. Sapiente blanditiis obcaecati natus, similique, repellendus ipsum ipsam dicta eos consequatur, distinctio soluta?"
+    }
+
+    const [indicadorAlgumaAlteracaoRealizada, setIndicadorAlgumaAlteracaoRealizada] = useState(false);
+
+    const [textoDaPublicacao, setTextoDaPublicacao] = useState<string | null>(null);
+
+    const [indicadorModalConfirmacaoDescartarAberto, setIndicadorModalConfirmacaoDescartarAberto] = useState(false);
+
+    //Este useEffect é responsável por resetar os estados toda vez que o modal for re-aberto.
+    useEffect(() => {
+        setIndicadorAlgumaAlteracaoRealizada(false);
+        setTextoDaPublicacao(publicacao.texto);
+    }, [modalAberto]);
+
+    useEffect(() => {
+        if (
+            textoDaPublicacao?.trim() !== publicacao.texto
+        ) {
+            setIndicadorAlgumaAlteracaoRealizada(true);
+        } else {
+            setIndicadorAlgumaAlteracaoRealizada(false);
+        }
+    }, [textoDaPublicacao]);
+
+    function publicar() {
+
+    }
+
+    function aoDigitarTexto(e: React.ChangeEvent<HTMLTextAreaElement>) {
+        setTextoDaPublicacao(e.target.value);
+    }
+
+    function aoConfirmarDescartarAlteracoes() {
+        fecharModal();
+    }
+
+    function fecharModalConfirmacaoDescartar() {
+        setIndicadorModalConfirmacaoDescartarAberto(false);
+    }
+
+    function clickFechar() {
+
+        if (indicadorAlgumaAlteracaoRealizada) {
+            setIndicadorModalConfirmacaoDescartarAberto(true);
+        } else {
+            fecharModal();
+        }
+    }
+
+    return (
+        <>
+            {
+                indicadorModalConfirmacaoDescartarAberto ?
+                    <ModalDeConfirmacao
+                        titulo='Descartar alterações?'
+                        mensagem='Deseja realmente descartar as alterações?'
+                        modalAberto={indicadorModalConfirmacaoDescartarAberto}
+                        fecharModal={fecharModalConfirmacaoDescartar}
+                        aoConfirmar={aoConfirmarDescartarAlteracoes}
+                    /> :
+                    ""
+            }
+
+
+            <div id="modalEditarPublicacaoCompartilhada__overlay">
+                <div id="modalEditarPublicacaoCompartilhada">
+                    <div id='modalEditarPublicacaoCompartilhada__tituloEBtnDeFechar'>
+                        <h3 id='modalEditarPublicacaoCompartilhada__tituloModal'>Editar publicação</h3>
+                        <button
+                            id='modalEditarPublicacaoCompartilhada__btnFechar'
+                            className='material-symbols-outlined'
+                            onClick={clickFechar}
+                        >close</button>
+                    </div>
+                    <div id='modalEditarPublicacaoCompartilhada__container'>
+                        <div id="modalEditarPublicacaoCompartilhada__nomeEPerfilDoUsuario">
+                            <img
+                                src="./imagensDinamicas/perfil.jpg"
+                                alt="Perfil usuário"
+                                id="modalEditarPublicacaoCompartilhada__perfilUsuario"
+                            />
+                            <p id="modalEditarPublicacaoCompartilhada__nomeUsuario">Pedro souza</p>
+                        </div>
+
+                        <div id='modalEditarPublicacaoCompartilhada__containerInputs'>
+                            <textarea
+                                id='modalEditarPublicacaoCompartilhada__campoTexto'
+                                className="modalEditarPublicacaoCompartilhada__campoTextoMaisBaixo"
+                                value={textoDaPublicacao ? textoDaPublicacao : ""}
+                                placeholder='No que você está pensando, Pedro?'
+                                onChange={aoDigitarTexto}
+                                spellCheck={false}
+                            ></textarea>
+
+                            <div id='modalEditarPublicacaoCompartilhada__containerPublicacao'>
+                                <Publicacao publicacaoCompartilhada />
+                            </div>
+
+                        </div>
+                        <button
+                            id='modalEditarPublicacaoCompartilhada__btnSalvar'
+                            disabled={!indicadorAlgumaAlteracaoRealizada}
+                            className={!indicadorAlgumaAlteracaoRealizada ? "modalEditarPublicacaoCompartilhada__btnSalvarInativo" : ""}
+                            onClick={publicar}
+                        >Salvar</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/components/PublicacaoCompartilhada/index.tsx
+++ src/components/PublicacaoCompartilhada/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PublicacaoCompartilhada.css';
 import InteracoesComAPublicacao from '../Publicacao/InteracoesComAPublicacao';
 import Comentarios from '../Publicacao/Comentarios';
 import Publicacao from '../Publicacao';
+import { useMediaQuery } from 'react-responsive';
+import { TAMANHO_DE_TELA_MOBILE } from '../../config';
+import { useNavigate } from 'react-router-dom';
+import ModalEditarPublicacao from '../ModalEditarPublicacao';
+import ModalEditarPublicacaoCompartilhada from '../ModalEditarPublicacaoCompartilhada';
 
 export default function PublicacaoCompartilhada() {
+
+    //Mock provisório que indica se a publicacao atual é do autor que está logado. Futuramente para obter esse dado deverá ser feita uma verificação com dados vindos do redux ou algo semelhante
+    const indicadorPublicacaoDoUsuarioLogado = true;
 
     const publicacao = {
         nomeAutor: "Pedro souza",
@@ -13,32 +21,72 @@ export default function PublicacaoCompartilhada() {
         texto: "Isto é uma verdadeira obra de arte!",
     }
 
+    const navigate = useNavigate();
+
+    const isMobile = useMediaQuery({ maxWidth: TAMANHO_DE_TELA_MOBILE });
+
+    const [indicadorModalEditarPublicacaoAberto, setIndicadorModalEditarPublicacaoAberto] = useState(false);
+
+    function abrirModalEditarPublicacao() {
+        if (isMobile) {
+            navigate('/editarPublicacao');
+        } else {
+            setIndicadorModalEditarPublicacaoAberto(true);
+        }
+    }
+
+    function fehcarModalEditarPublicacao() {
+        setIndicadorModalEditarPublicacaoAberto(false);
+    }
+
     return (
-        <div id='publicacaoCompartilhada'>
-            <div id='publicacaoCompartilhada__infoUsuario'>
-                <div id='publicacaoCompartilhada__infoUsuarioContainer'>
-                    <img src={publicacao.perfil} alt="Perfil usuário" id='publicacaoCompartilhada__perfil' />
-                    <div id='publicacaoCompartilhada__infoUsuarioContainer__divInfo'>
-                        <p id='publicacaoCompartilhada__tituloAutor'>
-                            <span>Pedro souza </span>
-                            compartilhou uma publicação
-                        </p>
-                        <p id='publicacaoCompartilhada__tempoDePublicacao'>Há {publicacao.tempoDePublicacao}</p>
+        <>
+            {
+                indicadorModalEditarPublicacaoAberto ?
+                    <ModalEditarPublicacaoCompartilhada
+                        fecharModal={fehcarModalEditarPublicacao}
+                        modalAberto={indicadorModalEditarPublicacaoAberto}
+                    /> :
+                    ""
+            }
+            <div id='publicacaoCompartilhada'>
+                <div id='publicacaoCompartilhada__infoUsuario'>
+                    <div id='publicacaoCompartilhada__infoUsuarioContainer'>
+                        <img src={publicacao.perfil} alt="Perfil usuário" id='publicacaoCompartilhada__perfil' />
+                        <div id='publicacaoCompartilhada__infoUsuarioContainer__divInfo'>
+                            <p id='publicacaoCompartilhada__tituloAutor'>
+                                <span>Pedro souza </span>
+                                compartilhou uma publicação
+                            </p>
+                            <p id='publicacaoCompartilhada__tempoDePublicacao'>Há {publicacao.tempoDePublicacao}</p>
+                        </div>
                     </div>
+                    {
+                        indicadorPublicacaoDoUsuarioLogado ?
+                            <button
+                                className='material-symbols-outlined'
+                                id='publicacao__btnEditarPublicacao'
+                                onClick={abrirModalEditarPublicacao}
+                            >edit</button> :
+                            ""
+                    }
                 </div>
+
+
+
+                <p id='publicacaoCompartilhada__texto'>{publicacao.texto}</p>
+
+                <div id='publicacaoCompartilhada__containerPublicacao'>
+                    <Publicacao publicacaoCompartilhada={true} />
+                </div>
+
+
+                <InteracoesComAPublicacao />
+
+                <div id='publicacaoCompartilhada__linhaDivisoria'></div>
+                <Comentarios />
             </div>
+        </>
 
-            <p id='publicacaoCompartilhada__texto'>{publicacao.texto}</p>
-
-            <div id='publicacaoCompartilhada__containerPublicacao'>
-                <Publicacao publicacaoCompartilhada={true} />
-            </div>
-
-
-            <InteracoesComAPublicacao />
-
-            <div id='publicacaoCompartilhada__linhaDivisoria'></div>
-            <Comentarios />
-        </div>
     )
 }
