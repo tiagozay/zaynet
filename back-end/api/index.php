352ee743eefab0394d3ab0ffa1526375f81333cb
Tiago zay <tiagozay@gmail.com>
2024-01-10 09:45:28 -0300

    Criando back-end
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

---
+++ back-end/api/index.php
@@ -0,0 +1,32 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+$caminho = $_GET['url'];
+
+switch ($method) {
+    case 'GET':
+        break;
+    case 'POST':
+
+        switch ($caminho) {
+            case 'usuarios':
+                require_once '../controlers/cadastrarUsuario.php';
+                break;
+            default:
+                http_response_code(404);
+                echo json_encode(['error' => 'Recusro não encontrado']);
+                break;
+        }
+
+        break;
+    case 'PUT':
+        break;
+    case 'DELETE':
+        break;
+    default:
+        http_response_code(405);
+        echo json_encode(['error' => 'Método não permitido']);
+        break;
+}
