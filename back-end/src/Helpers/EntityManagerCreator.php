b88a11b1e0a454faf73f26245dfff6586971ba7c
Tiago zay <tiagozay@gmail.com>
2024-01-10 17:04:42 -0300

    Instalando doctrine e criando EntityManagerCreator
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

---
+++ back-end/src/Helpers/EntityManagerCreator.php
@@ -0,0 +1,38 @@
+<?php
+// bootstrap.php
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+require_once "vendor/autoload.php";
+require_once __DIR__."/../../env.php";
+
+class EntityManagerCreator
+{
+    public static function create()
+    {
+
+        $host = getenv('DB_HOST');
+        $dbName = getenv('DB_DATABASE');
+        $user = getenv('DB_USERNAME');
+        $password = getenv('DB_PASSWORD');
+
+        // Create a simple "default" Doctrine ORM configuration for Attributes
+        $config = ORMSetup::createAttributeMetadataConfiguration(
+            paths: array(__DIR__ . "/.."),
+            isDevMode: true,
+        );
+
+        // configuring the database connection
+        $connection = DriverManager::getConnection([
+            'driver' => 'pdo_mysql',
+            'host' => $host,
+            'user' => $user,
+            'dbname' => $dbName,
+            'password' => $password
+        ], $config);
+
+        // obtaining the entity manager
+        return new EntityManager($connection, $config);
+    }
+}
