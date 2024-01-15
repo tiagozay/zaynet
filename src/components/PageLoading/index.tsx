4b0c1b3dfa494d8dbf1495ad270b8d96e4be5c2e
Tiago zay <tiagozay@gmail.com>
2024-01-15 18:50:30 -0300

    Criando página de carregamento do site
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

---
+++ src/components/PageLoading/index.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import './PageLoading.css';
+
+export default function PageLoading() {
+    return (
+        <section id='pageLoading__page'>
+            <header id='pageLoading__header'>
+
+            </header>
+            <div id='pageLoading__container'>
+                <div id='pageLoading__menuEsquerdo'>
+                    <ul id='pageLoading__menuEsquerdo__listaDeOpcoes'>
+                        <li id='pageLoading__menuEsquerdo__opcao'>
+                            <div id='pageLoading__menuEsquerdo__opcao__icone'></div>
+                            <div id='pageLoading__menuEsquerdo__opcao__nome'></div>
+                        </li>
+                        <li id='pageLoading__menuEsquerdo__opcao'>
+                            <div id='pageLoading__menuEsquerdo__opcao__icone'></div>
+                            <div id='pageLoading__menuEsquerdo__opcao__nome'></div>
+                        </li>
+                        <li id='pageLoading__menuEsquerdo__opcao'>
+                            <div id='pageLoading__menuEsquerdo__opcao__icone'></div>
+                            <div id='pageLoading__menuEsquerdo__opcao__nome'></div>
+                        </li>
+                        <li id='pageLoading__menuEsquerdo__opcao'>
+                            <div id='pageLoading__menuEsquerdo__opcao__icone'></div>
+                            <div id='pageLoading__menuEsquerdo__opcao__nome'></div>
+                        </li>
+                        <li id='pageLoading__menuEsquerdo__opcao'>
+                            <div id='pageLoading__menuEsquerdo__opcao__icone'></div>
+                            <div id='pageLoading__menuEsquerdo__opcao__nome'></div>
+                        </li>
+                        <li id='pageLoading__menuEsquerdo__opcao'>
+                            <div id='pageLoading__menuEsquerdo__opcao__icone'></div>
+                            <div id='pageLoading__menuEsquerdo__opcao__nome'></div>
+                        </li>
+                        <li id='pageLoading__menuEsquerdo__opcao'>
+                            <div id='pageLoading__menuEsquerdo__opcao__icone'></div>
+                            <div id='pageLoading__menuEsquerdo__opcao__nome'></div>
+                        </li>
+                        <li id='pageLoading__menuEsquerdo__opcao'>
+                            <div id='pageLoading__menuEsquerdo__opcao__icone'></div>
+                            <div id='pageLoading__menuEsquerdo__opcao__nome'></div>
+                        </li>
+                    </ul>
+                </div>
+                <div id='pageLoading__feed'>
+                    <div id='pageLoading__feed__publicacao'>
+                        <div id='pageLoading__feed__publicacao__containerInfo'>
+                            <div id="pageLoading__feed__publicacao__perfil"></div>
+                            <div id="pageLoading__feed__publicacao__nome"></div>
+                        </div>
+                    </div>
+                </div>
+                <div id='pageLoading__contatosChat'>
+                    <ul id='pageLoading__contatosChat__listaContatos'>
+                        <li id='pageLoading__contatosChat__contato'>
+                            <div id='pageLoading__contatosChat__contato__perfil'></div>
+                            <div id='pageLoading__contatosChat__contato__nome'></div>
+                        </li>
+                        <li id='pageLoading__contatosChat__contato'>
+                            <div id='pageLoading__contatosChat__contato__perfil'></div>
+                            <div id='pageLoading__contatosChat__contato__nome'></div>
+                        </li>
+                        <li id='pageLoading__contatosChat__contato'>
+                            <div id='pageLoading__contatosChat__contato__perfil'></div>
+                            <div id='pageLoading__contatosChat__contato__nome'></div>
+                        </li>
+                        <li id='pageLoading__contatosChat__contato'>
+                            <div id='pageLoading__contatosChat__contato__perfil'></div>
+                            <div id='pageLoading__contatosChat__contato__nome'></div>
+                        </li>
+                        <li id='pageLoading__contatosChat__contato'>
+                            <div id='pageLoading__contatosChat__contato__perfil'></div>
+                            <div id='pageLoading__contatosChat__contato__nome'></div>
+                        </li>
+                        <li id='pageLoading__contatosChat__contato'>
+                            <div id='pageLoading__contatosChat__contato__perfil'></div>
+                            <div id='pageLoading__contatosChat__contato__nome'></div>
+                        </li>
+                        <li id='pageLoading__contatosChat__contato'>
+                            <div id='pageLoading__contatosChat__contato__perfil'></div>
+                            <div id='pageLoading__contatosChat__contato__nome'></div>
+                        </li>
+                        <li id='pageLoading__contatosChat__contato'>
+                            <div id='pageLoading__contatosChat__contato__perfil'></div>
+                            <div id='pageLoading__contatosChat__contato__nome'></div>
+                        </li>
+                        <li id='pageLoading__contatosChat__contato'>
+                            <div id='pageLoading__contatosChat__contato__perfil'></div>
+                            <div id='pageLoading__contatosChat__contato__nome'></div>
+                        </li>
+                        <li id='pageLoading__contatosChat__contato'>
+                            <div id='pageLoading__contatosChat__contato__perfil'></div>
+                            <div id='pageLoading__contatosChat__contato__nome'></div>
+                        </li>
+                        <li id='pageLoading__contatosChat__contato'>
+                            <div id='pageLoading__contatosChat__contato__perfil'></div>
+                            <div id='pageLoading__contatosChat__contato__nome'></div>
+                        </li>                    
+                    </ul>
+                </div>
+            </div>
+
+        </section>
+    )
+}
