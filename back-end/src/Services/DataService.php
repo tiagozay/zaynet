820d91d248e9ca9b48305a34af75956c3794202c
Tiago zay <tiagozay@gmail.com>
2024-01-24 14:45:49 -0300

    Criando função de formatar data
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- back-end/src/Services/DataService.php
+++ back-end/src/Services/DataService.php
@@ -10,4 +10,19 @@ abstract class DataService
     {
         return new DateTime();
     }
+
+    public static function formataDataParaString(DateTime $dataHora): string
+    {
+        $meses = [
+            'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
+            'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'
+        ];
+
+        $dia = $dataHora->format('j');
+        $mes = $meses[(int)$dataHora->format('n') - 1];
+        $ano = $dataHora->format('Y');
+        $hora = $dataHora->format('H:i');
+
+        return "{$dia} de {$mes} de {$ano} às {$hora}";
+    }
 }
