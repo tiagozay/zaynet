24b45ec224dac61cc670b55b0fb9247b0a1e7956
Tiago zay <tiagozay@gmail.com>
2024-02-13 17:52:03 -0300

    Tornando ID do modelo da midia no front somente numérico
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- src/models/Publicacao/MidiaPublicacaoModel.ts
+++ src/models/Publicacao/MidiaPublicacaoModel.ts
@@ -1,9 +1,9 @@
 export class MidiaPublicacaoModel {
-  public id: number | null;
+  public id: number;
   public caminhoMidiaNormal: string;
   public caminhoMidiaMiniatura: string;
 
-  constructor(id: number | null, caminhoMidiaNormal: string, caminhoMidiaMiniatura: string) {
+  constructor(id: number, caminhoMidiaNormal: string, caminhoMidiaMiniatura: string) {
     this.id = id;
     this.caminhoMidiaNormal = caminhoMidiaNormal;
     this.caminhoMidiaMiniatura = caminhoMidiaMiniatura;
