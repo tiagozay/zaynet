c56078e3bf6bef7d6b3c02e21fb890329b6feee5
Tiago zay <tiagozay@gmail.com>
2024-02-20 14:21:07 -0300

    Adicio
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- src/contexts/PerfilUsuarioContext.tsx
+++ src/contexts/PerfilUsuarioContext.tsx
@@ -1,25 +1,52 @@
 import React, { ReactNode, createContext, useState } from "react";
 import { ConversaModel } from "../models/ConversaModel";
+import { Usuario } from "../models/Usuario";
+import { PublicacaoModel } from "../models/Publicacao/PublicacaoModel";
+import { PublicacaoCompartilhadaModel } from "../models/Publicacao/PublicacaoCompartilhadaModel";
 
 interface TypePerfilUsuarioContext
 {
     indicadorUsuarioCarregando: boolean,
-    setIndicadorUsuarioCarregando: React.Dispatch<React.SetStateAction<boolean>>
+    setIndicadorUsuarioCarregando: React.Dispatch<React.SetStateAction<boolean>>,
+    indicadorPublicacoesDoUsuarioCarregando: boolean,
+    setIndicadorPublicacoesDoUsuarioCarregando: React.Dispatch<React.SetStateAction<boolean>>,
+    usuario: Usuario | null,
+    setUsuario: React.Dispatch<React.SetStateAction<Usuario | null>>,
+    publicacoes: Array<PublicacaoModel | PublicacaoCompartilhadaModel>,
+    setPublicacoes: React.Dispatch<React.SetStateAction<Array<PublicacaoModel | PublicacaoCompartilhadaModel>>>
 }
 
 export const PerfilUsuarioContext = createContext<TypePerfilUsuarioContext>({
     indicadorUsuarioCarregando: false,
-    setIndicadorUsuarioCarregando: () => {}
+    setIndicadorUsuarioCarregando: () => {},
+    indicadorPublicacoesDoUsuarioCarregando: false,
+    setIndicadorPublicacoesDoUsuarioCarregando: () => {},
+    usuario: null,
+    setUsuario: () => {},
+    publicacoes: [],
+    setPublicacoes: () => {}
 }); 
 
 export default function PerfilUsuarioContextProvider({children}: {children: ReactNode}){
 
     const [indicadorUsuarioCarregando, setIndicadorUsuarioCarregando] = useState<boolean>(false);
 
+    const [indicadorPublicacoesDoUsuarioCarregando, setIndicadorPublicacoesDoUsuarioCarregando] = useState<boolean>(false);
+
+    const [usuario, setUsuario] = useState<Usuario | null>(null);
+    
+    const [publicacoes, setPublicacoes] = useState<Array<PublicacaoModel | PublicacaoCompartilhadaModel>>([]);
+
     return (
         <PerfilUsuarioContext.Provider value={{
             indicadorUsuarioCarregando,
             setIndicadorUsuarioCarregando,
+            indicadorPublicacoesDoUsuarioCarregando,
+            setIndicadorPublicacoesDoUsuarioCarregando,
+            usuario,
+            setUsuario,
+            publicacoes,
+            setPublicacoes
         }}>
             {children}
         </PerfilUsuarioContext.Provider>
