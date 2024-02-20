e888119a4ea855b333dfb4b3c86234739dc2281b
Tiago zay <tiagozay@gmail.com>
2024-02-20 11:39:21 -0300

    Criando funções que obtem caminho completo do perfil e capa de determinado usuário
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- src/services/UsuarioService.ts
+++ src/services/UsuarioService.ts
@@ -3,6 +3,7 @@ import { APIService } from "./APIService";
 import { LoginService } from "./LoginService";
 import APIResponse from "../Utils/APIResponse";
 import { ArquivosPublicacaoService } from "./ArquivosPublicacaoService";
+import { Usuario } from "../models/Usuario";
 
 export default abstract class UsuarioService {
     public static async cadastraUsuario(
@@ -85,5 +86,47 @@ export default abstract class UsuarioService {
             return `${process.env.REACT_APP_CAMINHO_IMAGEM_SEM_PERFIL}`;
         }
     
+    }
+
+    public static obtemCaminhoCompletoDoPerfilDoUsuario(usuario: Usuario): string
+    {
+        const nome = usuario.nomeFotoPerfil;
+
+        let caminho = process.env.REACT_APP_CAMINHO_IMAGEM_PERFIL;
+
+        if(!nome){
+            return process.env.REACT_APP_CAMINHO_IMAGEM_SEM_PERFIL as string;
+        }
+
+        return `${caminho}${nome}`;
+
+    }
+
+    public static obtemCaminhoCompletoDoPerfilMiniaturaDoUsuario(usuario: Usuario): string
+    {
+        const nome = usuario.nomeMiniaturaFotoPerfil;
+
+        let caminho = process.env.REACT_APP_CAMINHO_IMAGEM_PERFIL_MINIATURA;
+
+        if(!nome){
+            return process.env.REACT_APP_CAMINHO_IMAGEM_SEM_PERFIL as string;
+        }
+
+        return `${caminho}${nome}`;
+
+    }
+
+    public static obtemCaminhoCompletoDaCapaDoUsuario(usuario: Usuario): string | false
+    {
+        const nome = usuario.nomeCapa;
+
+        let caminho = process.env.REACT_APP_CAMINHO_IMAGEM_CAPA;
+
+        if(!nome){
+            return false;
+        }
+
+        return `${caminho}${nome}`;
+
     }
 }
