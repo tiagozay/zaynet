e7b73331751d9d6f420a64633be1d186ed91f9ed
Tiago zay <tiagozay@gmail.com>
2024-02-23 11:22:00 -0300

    Escrevendo as imagens na página de fotos do Usuário
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- src/pages/FotosPerfilUsuario/index.tsx
+++ src/pages/FotosPerfilUsuario/index.tsx
@@ -1,59 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './FotosPerfilUsuario.css';
 import { MidiaPublicacaoModel } from '../../models/Publicacao/MidiaPublicacaoModel';
 import { useNavigate } from 'react-router-dom';
+import { PerfilUsuarioContext } from '../../contexts/PerfilUsuarioContext';
+import { FeedContext } from '../../contexts/FeedContext';
 
 export default function FotosPerfilUsuario() {
 
   const navigate = useNavigate();
 
-  //Mock das fotos do usuário logado que futuramente virão de outro lugar, como redux
-  const fotosUsuario = [
-    new MidiaPublicacaoModel(1,
-      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub1.jpg',
-      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub1.jpg',
-    ),
-    new MidiaPublicacaoModel(2,
-      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub2.jpg',
-      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub2.jpg',
-    ),
-    new MidiaPublicacaoModel(3,
-      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub3.jpg',
-      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub3.jpg',
-    ),
-    new MidiaPublicacaoModel(4,
-      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub4.jpg',
-      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub4.jpg',
-    ),
-    new MidiaPublicacaoModel(5,
-      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub5.jpg',
-      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub5.jpg',
-    ),
-    new MidiaPublicacaoModel(6,
-      '/imagensDinamicas/publicacoes/imagensNormaisEVideos/pub6.mp4',
-      '/imagensDinamicas/publicacoes/miniaturasDasImagens/pub6.jpg',
-    )
-  ];
+  const { definePosicaoDoFeed } = useContext(FeedContext);
+
+  const {imagens} = useContext(PerfilUsuarioContext);
 
   function aoClicarEmUmaImagem(indice: number) {
-    const info = JSON.stringify(
-      { imagensDoCarrossel: fotosUsuario, indiceImagemInicial: indice }
-    );
-    navigate(`/image/${encodeURIComponent(info)}`);
-  }
 
+    const info = {
+      midias: imagens,
+      indiceInicial: indice
+    };
+
+    definePosicaoDoFeed(window.scrollY)
+      .then(() => { navigate('/image', { state: info }) });
+
+  }
 
   return (
     <section id="fotosPerfilDoUsuario__page">
       <h3 id="fotosPerfilDoUsuario__titulo">Fotos</h3>
       <ul id='fotosPerfilDoUsuario__listaDeFotos'>
-
         {
-          fotosUsuario.map((foto, index) => {
+          imagens.map((foto, index) => {
             return (
               <li className='fotosPerfilDoUsuario__listaDeFotos__foto' key={index}>
                 <img 
-                  src={foto.caminhoMidiaMiniatura}
+                  src={`${process.env.REACT_APP_CAMINHO_MIDIA_PUBLICACAO_MINIATURA}${foto.caminhoMidiaMiniatura}`}
                   alt="Foto usuário"
                   onClick={() => aoClicarEmUmaImagem(index)}
                 />
