47c1970a187d9859b4db1db1d0ff9f5b55dfb406
Tiago zay <tiagozay@gmail.com>
2024-02-23 14:13:00 -0300

    Ajuste comentário back-end
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- back-end/src/Controlers/excluirComentario.php
+++ back-end/src/Controlers/excluirComentario.php
@@ -7,7 +7,7 @@ use Tiagozay\BackEnd\Utils\APIResponse;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-//Verificação realizada para que o código não seja executado em outros momentos (como quando rodar a CLI do doctrine), aí rodará somente quando for uma requisição HTTP com o método POST
+//Verificação realizada para que o código não seja executado em outros momentos (como quando rodar a CLI do doctrine), aí rodará somente quando for uma requisição HTTP com o método DELETE
 if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === "DELETE") {
     $authorizationHeader = $_SERVER['HTTP_AUTHORIZATION'];
     $token = str_replace('Bearer ', '', $authorizationHeader);
